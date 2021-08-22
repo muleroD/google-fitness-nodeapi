8352849caabb1712af8abf4016c99088fee99578
Richard Mulero <mulerolp@gmail.com>
2021-08-22 19:28:13 -0300

    chore: update prettier file

--- config/google.js
+++ config/google.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: process.env.REDIRECT_URI,
+  redirectUri: process.env.REDIRECT_URI
 };
